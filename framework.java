e7d3eeb564a82c2057368ddba0b924775c7b86a0
Raj krishna <58541488+RajKrishna2123@users.noreply.github.com>
2022-03-29 14:43:43 +0530

    till 29/03/2022

--- framework.java
+++ framework.java
@@ -1,26 +1,38 @@
 import javax.swing.*;
-
+import java.awt.*;
 public class framework extends JFrame
 {
 	static JFrame f;
-	static JButton b1,b2,b3,b4,b5,b6;
+	static JButton b1;
 	static JLabel l;
 	public static void main(String args[] )
 	{
-		f= new JFrame("yo! thats me yo yo yo yo !!!! made it hahah 😂😂😂");
-		// JPanel ff=new JPanel();
-		b1= new JButton("raj");
-		l=  new JLabel("YO YO YO YO !!!!");
-		// l=new JLabel("YOYOYOYOYOY!!!!!!");
-		//setting visibility
-		// f.setTitle();
+		f= new JFrame("yo! thats me yo yo yo yo !!!! made it hahah 😂😂😂" );
+		l= new JLabel("YO YO YO YO !!!!",JLabel.RIGHT);
+		f.setContentPane(new JLabel(new ImageIcon("E://wallpaper/collection/3200x1600.jpg")));
+		b1= new JButton("raj");		
+		b1.setBounds(850,400,100,200);
+		
 		JPanel p = new JPanel();
+		JPanel q = new JPanel();
+		f.setLayout(new FlowLayout());
 		p.add(l);
-		p.add(b1);
+		p.setBackground(Color.ORANGE);
+		p.setOpaque(true); // make transparent background
+		p.setBackground(Color.LIGHT_GRAY);
+		p.setPreferredSize(new Dimension(250,150));
+
+		q.add(b1);
+		q.setBackground(new Color(0,0,0,65));
+		q.setPreferredSize(new Dimension(250,150));
+
 		
 		f.add(p);
+		f.add(q);
+
 		f.setSize(1750,850);  
-		f.setLayout(null);
+		//f.setLayout(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 		f.show();
 
--- testy.java
+++ testy.java
@@ -1,21 +1,57 @@
-import java.lang.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-public class testy
-{
-    public static void main(String[]args)
+import javax.swing.*;
+import java.awt.*;
+class testy extends JFrame
+{   
+    static JFrame f;
+	static JButton b1;
+	static JLabel l, l1;
+    public static void main(String args[])
     {
-        ArrayList <Integer>arr= new ArrayList<>();
-        for(int i=0;i<10;i++)
-        {
-            arr.add((int)(Math.random()*100));
-        }
-        System.out.println("Original List of random numbers:"+arr);
-        Collections.sort(arr);
-        System.out.println("Sorted List of random numbers"+arr);
-        Set arrSet=new HashSet(arr);
-        System.out.println("Sorted list of random numbers with duplicates removed,ifany:"+arrSet);
-    }
+        // Set title and default close operation
+        f= new JFrame();
+        f.setTitle("Transparent Panel");
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        // Set a background for JFrame
+        f.setContentPane(new JLabel(new ImageIcon("E://wallpaper/collection/3200x1600.jpg")));
+        
+        // Set some layout, say FlowLayout
+        f.setLayout(new FlowLayout());
+        l= new JLabel("YO YO YO YO !!!!",JLabel.RIGHT);
+        l1= new JLabel("YO YO YO YO gr8!!!!",JLabel.RIGHT);
+        l.setForeground(Color.GREEN);
+        l1.setForeground(Color.ORANGE);
+        //l.setBounds(0, 800, 120, 20);
+        l.setBounds(150, 100, 150, 50);
+        l1.setBounds(150, 100, 150, 50);
+
+        
+
+        JPanel p = new JPanel();
+        // Set the background, black with 125 as alpha value
+        // This is less transparent
+        p.add(l);
+        p.setBackground(new Color(0,0,0,125));
+        
+        // Create another JPanel
+        JPanel q=new JPanel();
+        
+        // This is more transparent than the previous
+        // one
+        q.add(l1);
+        q.setBackground(new Color(0,0,0,65));
+        
+        // Set some size to the panels
+        p.setPreferredSize(new Dimension(800,800));
+        q.setPreferredSize(new Dimension(800,800));
+        
+        // Add the panels to the JFrame
+        f.add(p);
+        f.add(q);
+        
+        // Set the size of the JFrame and
+        // make it visible
+        f.setSize(1750,850); 
+        f.setVisible(true);
+    }      
 }
