c12d2f126617c7cb1415ecfa3972a329dcebb425
Raj krishna <58541488+RajKrishna2123@users.noreply.github.com>
2022-03-29 14:43:28 +0530

    till 29/3/2022

---
+++ tempo2.java
@@ -0,0 +1,32 @@
+// import javax.swing.*;
+// import java.awt.*;
+// public class tempo2 extends JFrame
+// {
+// 	static JFrame f;
+// 	static JButton b1;
+//     static JPanel panel,topRow;
+// 	static JLabel l;
+// 	public static void main(String args[] )
+// 	{
+// 		panel = new JPanel(new BorderLayout());
+//         topRow = new JPanel();
+//         l=  new JLabel("YO YO YO YO !!!!");
+//         panel.add(topRow, BorderLayout.PAGE_START);
+//         topRow.add(l);
+//         // topRow.add(area1);
+//         JPanel middleRowBox = new JPanel(new BorderLayout());
+//         panel.add(middleRowBox, BorderLayout.CENTER);
+//         JPanel middleRow = new JPanel();
+//         middleRowBox.add(middleRow, BorderLayout.PAGE_START);
+//         middleRow.add(l);
+//         // middleRow.add(area2);
+//         JPanel bottomRowBox = new JPanel();
+//         middleRowBox.add(bottomRowBox, BorderLayout.CENTER);
+//         JPanel bottomRow = new JPanel();
+//         bottomRowBox.add(bottomRow, BorderLayout.PAGE_START);
+//         bottomRow.add(l);
+//         // bottomRow.add(area3);
+//         bottomRowBox.add(new JPanel(), BorderLayout.CENTER);    
+// 	}
+
+// }
